01f9629cf7e35eae6261296bb57d1f6eb01dac15
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-02-02 16:50:22 -0600

    added setup files to the project
    public
    routes
    views

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes/main');
+
 app.listen(3000 , () => {
 
     console.log('The express site application is running on localhost:3000!');
