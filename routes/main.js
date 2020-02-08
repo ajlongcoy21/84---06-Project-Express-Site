9c4f328e286def76288e00577af4600da18ad0a4
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-02-08 16:28:23 -0600

    Dynamic pug file with JSON data
    updated the projectData.json file to include the values needed to make the index.pug file dynamic. Also updated the main.js to include the JSON data and pass it to the index.pug file to use.

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {data} = require('../data/projectData.json');
+const portfolioProjects = data.projects;
 
 router.get('/',(req, res) => {
     
-    res.render('index');    
+    res.render('index', {portfolioProjects: portfolioProjects});
+    console.log("here now");
+    console.log(portfolioProjects[0].image_urls[0]);
+        
     
 });
 
